61931b6dcc4596db92278fe7aeaf4504c8ccc81a
joaovitorlf <joaovitorl.ferreira@hotmail.com>
2022-03-25 10:34:46 -0300

    Integração BootStrap

--- Gemfile
+++ Gemfile
@@ -3,6 +3,10 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby "3.1.1"
 
+gem "bootstrap-sass", "~> 3.4.1"
+gem 'sass-rails', '~> 6.0'
+gem 'bootstrap', '~> 5.1.3'
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.2", ">= 7.0.2.3"
 
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "jquery"
+import "jquery_ujs"
+import "bootstrap-sprockets"
+import "_tree"
+import "turbolinks"
+
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require turbolinks
+//= require tree .
